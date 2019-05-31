d69e3eabf0d5eb92b777c0cc5f55c4867af040f1
GiacomoPappaderi <danieleangileri2002@gmail.com>
2019-05-31 09:57:04 +0200

    commit in master conflitto

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ void main(){
         contatore=1;
         cout<<contatore<<endl;
         cout<"Voglio creare un conflitto"<<endl;
+        conflito
+        --- 
+        conflitto
         }
         system ("pause");
         }
