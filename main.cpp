d1ac4ca5bf46b193f0687124e2152106633bf3d1
GiacomoPappaderi <danieleangileri2002@gmail.com>
2019-05-31 09:36:45 +0200

    Primo commit ora inserisco u conflitto

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*Giacomo pappaderi 3BIA 31/05/2019*/
+#include <iostream>
+using namespace std;
+void main(){
+    int contatore = 0
+    for (int 1=0;int i<11){
+        contatore=1;
+        cout<<contatore<<endl;
+        }
+        system ("pause");
+        }
